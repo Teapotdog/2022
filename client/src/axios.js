308231b410eb5efa74daec055d2468a050ccb1fc
Teapotdog <141807264+Teapotdog@users.noreply.github.com>
2024-12-18 23:11:27 +1030

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://byd-dvaeh0d5bwh7g5a0.australiaeast-01.azurewebsites.net",
   withCredentials: true,
 });
