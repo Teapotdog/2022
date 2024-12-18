32ea280bf687a9cc90c45ae088d7d77232a83c7d
Teapotdog <141807264+Teapotdog@users.noreply.github.com>
2024-12-19 00:01:10 +1030

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://ashy-wave-0b2044d00.4.azurestaticapps.net",
   })
 );
 app.use(cookieParser());
