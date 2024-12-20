fa9d9f44e3978e954638e0c4202a875a41e82b11
teapotcat <teapotcat@outlook.com>
2024-12-20 18:25:03 +1030

    fixed

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://byd-dvaeh0d5bwh7g5a0.australiaeast-01.azurewebsites.net",
+  baseURL: "http://byd-dvaeh0d5bwh7g5a0.australiaeast-01.azurewebsites.net/routes",
   withCredentials: true,
 });
-fetch("https://byd-dvaeh0d5bwh7g5a0.australiaeast-01.azurewebsites.net", {
+fetch("http://byd-dvaeh0d5bwh7g5a0.australiaeast-01.azurewebsites.net/routes", {
   method: "GET",
   credentials: "include", // 允许发送 Cookies
 })
@@ -15,3 +15,12 @@ fetch("https://byd-dvaeh0d5bwh7g5a0.australiaeast-01.azurewebsites.net", {
   .catch((error) => {
     console.error("Error fetching data:", error);
   });
+
+  makeRequest.get("http://byd-dvaeh0d5bwh7g5a0.australiaeast-01.azurewebsites.net/routes")
+  .then((response) => {
+    console.log("User data:", response.data);
+  })
+  .catch((error) => {
+    console.error("Error fetching users:", error);
+  });
+
